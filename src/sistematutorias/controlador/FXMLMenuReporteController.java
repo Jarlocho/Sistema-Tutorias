b79e7330b22045b9b33d32a3a071d4077eea5f6f
Jarlocho <jarlyhdezromero@hotmail.com>
2025-11-24 12:38:14 -0600

    Ventanas y Enmaquetación básica del sistema

---
+++ src/sistematutorias/controlador/FXMLMenuReporteController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package sistematutorias.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author HP
+ */
+public class FXMLMenuReporteController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clicGenerarReporteTutoria(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicEnviarReporteTutoria(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicResponderReporteTutoria(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicGenerarReporteGeneral(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicRevisarReporteGeneral(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicResponderReporteGeneral(ActionEvent event) {
+    }
+    
+}
---
+++ src/sistematutorias/controlador/FXMLMenuTutoriaController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package sistematutorias.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author HP
+ */
+public class FXMLMenuTutoriaController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clicRegistrarHoraTutoria(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicRegistrarAsistenciaTutorado(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicRegistrarFechaTutoria(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicAsignarTutorado(ActionEvent event) {
+    }
+    
+}
